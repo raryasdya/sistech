6d7c37c3d94faee0b8955563734c536d8487507f
Fairuza Raryasdya A <fairuza.raryasdya@gmail.com>
2022-07-26 18:15:14 +0700

    feat: lead, work, achievement section

---
+++ constants.js
@@ -0,0 +1,91 @@
+export const contact = [
+  {
+    name: "linkedin",
+    link: "https://linkedin.com/in/raryasdya",
+  },
+  {
+    name: "mail",
+    link: "mailto:fairuza.raryasdya@ui.ac.id",
+  },
+  {
+    name: "github",
+    link: "https://github.com/raryasdya",
+  },
+  {
+    name: "gitlab",
+    link: "https://gitlab.com/raryasdya",
+  },
+]
+
+export const workExperience = [
+  {
+    role: "Software Engineer Intern",
+    company: "Kargo Technologies",
+    companyLink: "https://kargo.tech/",
+    date: "Jun 2022 - present",
+    details: [
+      "Automated bulk invoices creation, <b>speeding up the previous process down to under a minute</b>.",
+      "<b>Working closely with the operational teams</b> to develop an automated legal agreement documents builder.",
+    ],
+    stacks: ["React", "Next.js", "Apps Script",]
+  },
+  {
+    role: "Software Engineer Intern",
+    company: "GMF AeroAsia",
+    companyLink: "https://www.gmf-aeroasia.co.id/",
+    date: "Feb 2022 - Mar 2022",
+    details: [
+      "Identified critical codes that <b>improved running time by more than 300 times faster</b>.",
+      "Designed and implemented a monitoring system that includes project monitoring, document tracking, and budget realization for external cooperation usage from scratch.",
+    ],
+    stacks: ["Vue.js", "Laravel", "CodeIgniter", "MySQL",]
+  },
+  {
+    role: "Software Engineer Intern",
+    company: "Dekoruma",
+    companyLink: "https://www.dekoruma.com/",
+    date: "Dec 2021 - Feb 2022",
+    details: [
+      "<b>Improved traffic and SEO</b> by creating Top Categories section and standardized footer across 3 websites.",
+      "Automated mood board duplication process on the internal microsite to <b>make the process instantaneous</b>.",
+      "Developed <a href='https://www.thudio.com/' style=\"color: '#83a484;'\">Thudio</a> (SketchUp extension) <b>Paint Features</b> that include color categorization, picking different paint products for each color, as well as identifying color and its paint product on existing assets.",
+    ],
+    stacks: ["React", "TypeScript", "Django", "Ruby", "PostgreSQL", "Algolia",]
+  },
+]
+
+export const achievements = [
+  {
+    title: "Awardee",
+    event: "Beasiswa Unggulan",
+    year: "2020",
+    detail: "A scholarship for students who achieve accomplishments that contribute to Indonesia. The scholarship covers tuition fees and living expenses until graduates."
+  },
+  {
+    title: "Silver Medalist",
+    event: "National Olympiad in Informatics",
+    year: "2018",
+    detail: "The most prestigious olympiad organized by the Ministry of Education and Culture of Indonesia with <b>more than 1500 participants from all over Indonesia</b>, where the final round used the IOI-style competitive programming competition."
+  },
+]
+
+export const organizationExperience = [
+  {
+    role: "Person in Charge of AI Innovation Challenge",
+    organization: "COMPFEST",
+    date: "Oct 2020 - Nov 2021",
+    details: [
+      "Led a team of 11 to hold <b>the first AI-Hackathon by and for Indonesian students</b> with 130+ participants.",
+      "<b>Established cooperation with Kampus Merdeka and 5 leading companies</b> for a speaker, mentors, judges, financial resources, and technical resources that were given for free for all participants.",
+    ]
+  },
+  {
+    role: "Head of Public Relations",
+    organization: "DPM Fasilkom UI",
+    date: "Jan 2020 - Jan 2021",
+    details: [
+      "With a team of 5, centralized every DPM’s public document to ease public’s access.",
+      "Raised awareness of DPM’s activities by posting 300+ publications, thus resulting in <b>doubling the number of followers</b> in a year.",
+    ]
+  },
+]
